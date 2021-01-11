75b4e3086710c0f10a380d1e9ca8b276b9f8d0b2
pstromberg98 <parker.stromberg@gmail.com>
2021-01-11 13:35:35 -0700

    Added animated bouncy list

--- lib/flutter_bouncy.dart
+++ lib/flutter_bouncy.dart
@@ -1,4 +1,5 @@
 library flutter_bouncy;
 
 export 'sliver_bouncy_list.dart';
+export 'sliver_animated_bouncy_list.dart';
 export 'bouncy_list.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,3 @@
 export 'sliver_bouncy_list.dart';
+export 'sliver_animated_bouncy_list.dart';
 export 'bouncy_list.dart';
